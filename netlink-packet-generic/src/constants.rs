8c9f6cc867859a1d2fb4818f10b0b14496ff91d5
Leo <leo881003@gmail.com>
2021-02-25 17:08:35 +0800

    Import more CTRL_ATTR constant from libc

--- netlink-packet-generic/src/constants.rs
+++ netlink-packet-generic/src/constants.rs
@@ -11,5 +11,28 @@ pub const CTRL_CMD_GETOPS: u8 = libc::CTRL_CMD_GETOPS as u8;
 pub const CTRL_CMD_NEWMCAST_GRP: u8 = libc::CTRL_CMD_NEWMCAST_GRP as u8;
 pub const CTRL_CMD_DELMCAST_GRP: u8 = libc::CTRL_CMD_DELMCAST_GRP as u8;
 pub const CTRL_CMD_GETMCAST_GRP: u8 = libc::CTRL_CMD_GETMCAST_GRP as u8;
-//pub const CTRL_CMD_GETPOLICY: u8 = libc::CTRL_CMD_GETPOLICY as u8;
 pub const CTRL_CMD_GETPOLICY: u8 = 10;
+
+pub const CTRL_ATTR_UNSPEC: u16 = libc::CTRL_ATTR_UNSPEC as u16;
+pub const CTRL_ATTR_FAMILY_ID: u16 = libc::CTRL_ATTR_FAMILY_ID as u16;
+pub const CTRL_ATTR_FAMILY_NAME: u16 = libc::CTRL_ATTR_FAMILY_NAME as u16;
+pub const CTRL_ATTR_VERSION: u16 = libc::CTRL_ATTR_VERSION as u16;
+pub const CTRL_ATTR_HDRSIZE: u16 = libc::CTRL_ATTR_HDRSIZE as u16;
+pub const CTRL_ATTR_MAXATTR: u16 = libc::CTRL_ATTR_MAXATTR as u16;
+pub const CTRL_ATTR_OPS: u16 = libc::CTRL_ATTR_OPS as u16;
+pub const CTRL_ATTR_MCAST_GROUPS: u16 = libc::CTRL_ATTR_MCAST_GROUPS as u16;
+pub const CTRL_ATTR_POLICY: u16 = 8;
+pub const CTRL_ATTR_OP_POLICY: u16 = 9;
+pub const CTRL_ATTR_OP: u16 = 10;
+
+pub const CTRL_ATTR_OP_UNSPEC: u16 = libc::CTRL_ATTR_OP_UNSPEC as u16;
+pub const CTRL_ATTR_OP_ID: u16 = libc::CTRL_ATTR_OP_ID as u16;
+pub const CTRL_ATTR_OP_FLAGS: u16 = libc::CTRL_ATTR_OP_FLAGS as u16;
+
+pub const CTRL_ATTR_MCAST_GRP_UNSPEC: u16 = libc::CTRL_ATTR_MCAST_GRP_UNSPEC as u16;
+pub const CTRL_ATTR_MCAST_GRP_NAME: u16 = libc::CTRL_ATTR_MCAST_GRP_NAME as u16;
+pub const CTRL_ATTR_MCAST_GRP_ID: u16 = libc::CTRL_ATTR_MCAST_GRP_ID as u16;
+
+pub const CTRL_ATTR_POLICY_UNSPEC: u16 = 0;
+pub const CTRL_ATTR_POLICY_DO: u16 = 1;
+pub const CTRL_ATTR_POLICY_DUMP: u16 = 2;
